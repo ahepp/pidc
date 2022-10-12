fdc1b3aca72f39105fb8feccbd0b1338a9f66960
Andrew.Hepp <git@ahepp.dev>
2022-10-11 21:48:41 -0700

    add pid_sim

---
+++ test/pidc_sim.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "pidc.h"
+
+int main(int argc, char **argv) {
+    if (argc != 4) {
+        return 1;
+    }
+    int32_t kp = atoi(argv[1]);
+    int32_t ki = atoi(argv[2]);
+    int32_t kd = atoi(argv[3]);
+
+    pidc_t *pidc;
+    pidc_create(&pidc, kp, ki, kd);
+    while (1) {
+        int32_t e;
+        scanf("%d", &e);
+        int32_t gain = pidc_update(pidc, e);
+        printf("error %d results in gain %d\n", e, gain);
+    }
+    return 0;
+}
